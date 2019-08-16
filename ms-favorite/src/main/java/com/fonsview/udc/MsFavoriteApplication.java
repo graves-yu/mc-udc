181416ecfe482acea1353a5cc4fe19cd75943dc8
graves-yu <269811485@qq.com>
2019-08-16 09:08:44 +0800

    bootstrap文件修改

--- ms-favorite/src/main/java/com/fonsview/udc/MsFavoriteApplication.java
+++ ms-favorite/src/main/java/com/fonsview/udc/MsFavoriteApplication.java
@@ -2,10 +2,12 @@ package com.fonsview.udc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class MsFavoriteApplication {
 
 	public static void main(String[] args) {
